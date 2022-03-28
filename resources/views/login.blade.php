6bca692201aac85084717a585fef7fe6cc3f6648
jatingitlaravel <jatinkhatri125@gmail.com>
2022-03-28 10:44:36 +0530

    login changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,6 +12,10 @@
     <label for="exampleInputPassword1">Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
   </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Massage</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
 
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
