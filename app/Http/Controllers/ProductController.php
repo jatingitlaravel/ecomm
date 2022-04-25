21d858374fcb21c5a9b77a19be5b1feaad833f7a
jatingitlaravel <jatinkhatri125@gmail.com>
2022-04-25 13:14:58 +0530

    logout and login done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
+use Session;
 
 class ProductController extends Controller
 {
     //
-    function index()
+    function index()    
     {
         $data= Product::all();
         return view('product',['products'=>$data]);
@@ -22,6 +24,25 @@ class ProductController extends Controller
 
     function addtoCart(Request $req)
     {
-        return "hello";
+        if ($req->session()->has('user'))
+        {
+                $cart= new Cart;
+                $cart->user_id=$req->session()->get('user')['id'];
+                $cart->product_id=$req->product_id;
+                $cart->save();
+                return redirect('/');
+
+        }
+        else
+        {
+              return redirect('/login');
+        }
     }
+
+     static function cartItem()
+    {
+        $userId=Session::get('user')['id'];
+        return Cart::where('user_id',$userId)->count();
+    }
+    
 }
